ee43bd3947a44c3aead0b13e17f95f70fbb4aa0f
Elkenzi <moelkenzi@gmail.com>
2025-09-15 12:36:59 +0300

    (feat) - added ShadcnUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello From The Staff Management System!</h1>;
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello From The Staff Management System!</h1>
+      <Button>Hello</Button>
+    </main>
+  );
 }
